16327bf3546cd729892c9051f82435a18b49e325
Mahmoud Ahmadi <ma.ahmadi1989@gmail.com>
2025-08-29 15:23:33 +0330

    fix: expose save and load function to be used in other packages

--- internal/repository/file/storage.go
+++ internal/repository/file/storage.go
@@ -1,15 +1,14 @@
-package main
+package filestorage
 
 import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"task-manager/internal/repository/memorycache"
 )
 
-var task_map = map[int]Task{}
-
-func saveTasks() {
-	data, err := json.MarshalIndent(task_map, "", "  ")
+func SaveTasks() {
+	data, err := json.MarshalIndent(memorycache.Tasks, "", "  ")
 	if err != nil {
 		fmt.Println("saving failed: ", err)
 		return
@@ -17,12 +16,16 @@ func saveTasks() {
 	os.WriteFile("Save-json/tasks.json", data, 0644)
 }
 
-func loadTask() {
+func LoadTask() {
 	file, err := os.ReadFile("Save-json/tasks.json")
 	if err != nil {
 		fmt.Println("Loading failed: ", err)
 		return
 	}
 
-	json.Unmarshal(file, &task_map)
+	if err := json.Unmarshal(file, &memorycache.Tasks); err != nil {
+		fmt.Println("Unmarshalling failed: ", err)
+		return
+	}
+
 }
